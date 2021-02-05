6524bd2e6c7b30cfe0c67432db887a20c60c946a
John Li <hud_johnli@live.co.uk>
2021-02-05 21:07:42 +0000

    project complete - added functionality to delete individual items from list

---
+++ src/List.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const List = ({ people, setPeople }) => {
+  const deleteHandler = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+  };
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <span>
+                <h4>{name}</h4>
+                <p>{age} years</p>
+              </span>
+
+              <button className="btn2" onClick={() => deleteHandler(id)}>
+                Clear
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;
