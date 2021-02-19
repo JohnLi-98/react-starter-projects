75f3a695fe27af27fcffd6636345cb3c813b2d6b
John Li <hud_johnli@live.co.uk>
2021-02-19 18:35:51 +0000

    completed the fourteenth project after fetching data with useReducer hook and creating a reducer function for toggling the amount in the cart

--- src/reducer.js
+++ src/reducer.js
@@ -2,14 +2,12 @@ const reducer = (state, action) => {
   if (action.type === "CLEAR_CART") {
     return { ...state, cart: [] };
   }
-
   if (action.type === "REMOVE") {
     return {
       ...state,
       cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
     };
   }
-
   if (action.type === "INCREASE") {
     let tempCart = state.cart.map((cartItem) => {
       if (cartItem.id === action.payload) {
@@ -22,7 +20,6 @@ const reducer = (state, action) => {
       cart: tempCart,
     };
   }
-
   if (action.type === "DECREASE") {
     let tempCart = state.cart
       .map((cartItem) => {
@@ -38,7 +35,25 @@ const reducer = (state, action) => {
       cart: tempCart,
     };
   }
-
+  if (action.type === "TOGGLE_AMOUNT") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === "inc") {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+          if (action.payload.type === "dec") {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return {
+      ...state,
+      cart: tempCart,
+    };
+  }
   if (action.type === "GET_TOTAL") {
     let { total, amount } = state.cart.reduce(
       (cartTotal, cartItem) => {
@@ -56,7 +71,13 @@ const reducer = (state, action) => {
     total = parseFloat(total.toFixed(2));
     return { ...state, total, amount };
   }
-  return state;
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload, loading: false };
+  }
+  throw new Error("No matching action type");
 };
 
 export default reducer;
