49613c93c2cfa209f2732b5ff62b088565f18311
John Li <hud_johnli@live.co.uk>
2021-02-21 17:49:49 +0000

    completed the fifteenth project

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur
+        cupiditate, sapiente quis, mollitia voluptatibus in adipisci
+        exercitationem voluptatum nisi enim officia vel, tempora at nemo
+        aspernatur quia ea dolorem dignissimos.
+      </p>
+    </section>
+  );
+};
+
+export default About;
